54c802c8245ac2ddf1ed3716ec7378f85844f768
Auroaruora <zw2985@columbia.edu>
2024-11-03 18:09:50 -0500

    Creating the signin and sign up UI

---
+++ InstaNutri/InstaNutri/App/InstaNutriApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaNutriApp.swift
+//  InstaNutri
+//
+//  Created by Aurora Weng on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaNutriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstaNutri/InstaNutri/Core/Authentication/RegistrationView.swift
@@ -0,0 +1,91 @@
+//
+//  RegistrationView.swift
+//  InstaNutri
+//
+//  Created by Aurora Weng on 11/3/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    @State var email = ""
+    @State var fullname = ""
+    @State var password = ""
+    @State var confirmPassword = ""
+    @Environment(\.dismiss) var dismiss
+    
+    //TODO, try to use @State private var email = "" but not working
+    var body: some View {
+        
+        
+        
+        NavigationStack{
+            VStack{
+                //image
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:200)
+                    .padding(.vertical,32)
+                
+                //from fields
+                VStack(spacing:24){
+                    InputView(text: $email,
+                              title: "Email Address",
+                              placeholder: "name@example.com")
+                    .autocapitalization(.none)
+                    
+                    InputView(text: $fullname,
+                              title: "Full Name",
+                              placeholder: "Enter your name")
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "Enter your password",
+                              isSecureField:true)
+                    
+                    InputView(text: $confirmPassword,
+                              title: " Confirm Password",
+                              placeholder: "Confirm your password",
+                              isSecureField:true)
+                    
+                }
+                .padding(.horizontal)
+                .padding(.top,12)
+                
+                //sign in button
+                Button{
+                    print("Sign user up..")
+                }label:{
+                    HStack{
+                        Text("SIGN UP")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundStyle(.white)
+                    .frame(width:UIScreen.main.bounds.width-32,height:48)
+                }
+                .background(Color(.systemBlue))
+                .cornerRadius(10)
+                .padding(.top,24)
+                
+                Spacer()
+                
+                Button{
+                    dismiss()
+                }label:{
+                    HStack(spacing:3){
+                        Text("Already have an account?")
+                        Text("Sign in")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size:14))
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
